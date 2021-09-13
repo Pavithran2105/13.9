5d415696aa971fbbbd90f955510b90674b50b03d
Pavithran2105 <88871384+Pavithran2105@users.noreply.github.com>
2021-09-13 14:27:53 +0530

    Update form3.js

--- form3.js
+++ form3.js
@@ -1,1 +1,43 @@
+var userName = document.getElementById("userName");
+var gmail = document.getElementById("gmail");
+var password = document.getElementById("password");
+var password2 = document.getElementById("password2");
+var form = document.getElementById("form");
+
+function showError(input){
+    const formControl = input;
+    // formControl.classList.add("failure");
+    formControl.className = "failure"; 
+
+}
+function showSuccess(input){
+    const formControl = input;
+    formControl.className = "success"
+
+}
+
+
+function checkAll(allInput){
+
+    for (let index = 0; index < allInput.length; index++) {
+        const element = allInput[index];
+        if (element.value === ""){
+            showError(element)
+        }
+        else{
+            showSuccess(element)
+        }
+        
+    }
+
+}
+
+form.addEventListener( 'submit', function (e){
+    e.preventDefault();
+    checkAll(allInput[userName,gmail,password,password2]);
+})
+
+    
+
+
 
