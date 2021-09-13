87fe99a9d465c848c25d7d659567e941b83af779
Pavithran2105 <88871384+Pavithran2105@users.noreply.github.com>
2021-09-13 14:27:09 +0530

    Update form2.js

--- form2.js
+++ form2.js
@@ -1,1 +1,60 @@
+var userName = document.getElementById("userName");
+var gmail = document.getElementById("gmail");
+var password = document.getElementById("password");
+var password2 = document.getElementById("password2");
+var form = document.getElementById("form");
+
+function showError(input){
+    const formControl = input;
+    // formControl.classList.add("failure");
+    formControl.className = "failure"; 
+
+}
+function showSuccess(input){
+    const formControl = input;
+    formControl.className = "success"
+
+}
+
+
+form.addEventListener( 'submit', function (e){
+    e.preventDefault();
+    if (userName.value === ''){
+        
+        showError(userName)
+    }
+    else{
+        
+        showSuccess(userName)
+    }
+    if (gmail.value === ''){
+        
+        showError(gmail)
+    }
+    else{
+        
+        showSuccess(gmail)
+    }
+    if (password.value === ''){
+        
+        showError(password)
+    }
+    else{
+        
+        showSuccess(password)
+    }   
+
+    if (password2.value === ''){
+        
+        showError(password2)
+    }
+    else{
+        
+        showSuccess(password2)
+    }
+})
+
+    
+
+
 
